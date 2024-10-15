0b7c0711e7e12c005d335885a846ae8187087244
Asneh Raj <rajasneh91@gmail.com>
2024-10-15 22:06:24 +0530

    Added index,show,create Routes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const MONGO_URL="mongodb://127.0.0.1:27017/staysphere";
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
-
+app.use(express.urlencoded({extended:true}));
 
 async function main(){
     await mongoose.connect(MONGO_URL);
@@ -29,9 +29,33 @@ app.get("/",(req,res)=>{
     res.send("Working");
 });
 
+
+//index route
 app.get("/listings",async (req,res)=>{
     const allListings=await Listing.find({});
-    res.render("/listings/index.ejs",{allListings});
+    res.render("listings/index.ejs",{allListings});
+});
+
+
+//new route
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs")
+});
+
+
+//Show route
+app.get("/listings/:id",async (req,res)=>{
+    let {id} =req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
+});
+
+
+//create route
+app.post("/listings",async (req,res)=>{
+    const newListing=new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings")
 });
 
 
