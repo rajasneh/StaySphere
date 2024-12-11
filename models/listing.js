c6afebedae9183cb8876dd5726724cce7a6e0684
Asneh Raj <rajasneh91@gmail.com>
2024-12-11 18:15:37 +0530

    Creating reviews model

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const review = require("./review");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -32,7 +33,13 @@ const listingSchema = new Schema({
     },
     location: {
         type: String,
-    }
+    },
+    reviews:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Review"
+        }
+    ]
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
