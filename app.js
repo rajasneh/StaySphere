19f744f3df5f8be78b34eb34db5c27cdffe1385e
Asneh Raj <rajasneh91@gmail.com>
2024-12-16 21:13:20 +0530

    Restructuring reviews using express router

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const {listingSchema,reviewSchema}=require("./schema.js");
 const Review=require("./models/review.js");
 const { wrap } = require("module");
 
+const listings=require("./routes/listing.js");
+
 const MONGO_URL="mongodb://127.0.0.1:27017/staysphere";
 
 
@@ -40,15 +42,7 @@ app.get("/",(req,res)=>{
 });
 
 
-const validateListing=(req,res,next)=>{
-    let {error}=listingSchema.validate(req.body);
-    if(error){
-        let errMsg=error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    }else{
-        next();
-    }
-}
+
 
 const validateReview=(req,res,next)=>{
     let {error}=reviewSchema.validate(req.body);
@@ -60,58 +54,8 @@ const validateReview=(req,res,next)=>{
     }
 }
 
+app.use("/listings",listings);
 
-//index route
-app.get("/listings",wrapAsync(async (req,res)=>{
-    const allListings=await Listing.find({});
-    res.render("listings/index.ejs",{allListings});
-}));
-
-
-//new route
-app.get("/listings/new",(req,res)=>{
-    res.render("listings/new.ejs")
-});
-
-
-//Show route
-app.get("/listings/:id",wrapAsync(async(req,res)=>{
-    let {id} =req.params;
-    const listing=await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs",{listing});
-}));
-
-
-//create route
-app.post("/listings",validateListing,wrapAsync(async (req,res,next)=>{
-        const newListing=new Listing(req.body.listing);
-        await newListing.save();
-        res.redirect("/listings")
-}));
-
-
-//edit route
-app.get("/listings/:id/edit",wrapAsync(async (req,res)=>{
-    let {id} =req.params;
-    const listing=await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing})
-}));
-
-//update route
-app.put("/listings/:id",validateListing,wrapAsync(async (req,res)=>{
-    let {id}=req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect("/listings");
-}));
-
-
-//delete route
-app.delete("/listings/:id",wrapAsync(async(req,res)=>{
-    let {id}=req.params;
-    let deletedListing=await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-}));
 
 //REVIEWS
 
