6748f19cb1b6ca278411fa4a961571553fc4adb6
Asneh Raj <rajasneh91@gmail.com>
2024-12-20 21:45:06 +0530

    Login user

--- routes/user.js
+++ routes/user.js
@@ -1,26 +1,41 @@
-const express=require("express");
-const router=express.Router();
-const User=require("../models/user.js");
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
 
-router.get("/signup",(req,res)=>{
+router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
 });
 
 
-router.post("/signup",wrapAsync(async(req,res)=>{
-    try{
-        let {username,email,password}=req.body;
-        const newUser=new User({email,username});
-        const registeresUser=await User.register(newUser,password);
+router.post("/signup", wrapAsync(async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new User({ email, username });
+        const registeresUser = await User.register(newUser, password);
         console.log(registeresUser);
-        req.flash("success","Welcome to StaySphere!");
+        req.flash("success", "Welcome to StaySphere!");
         res.redirect("/listings");
-    } catch(e){
-        req.flash("error",e.message);
+    } catch (e) {
+        req.flash("error", e.message);
         res.redirect("/signup");
     }
-    
+
 }));
 
-module.exports=router;
+router.get("/login", (req, res) => {
+    res.render("users/login.ejs")
+});
+
+router.post("/login", 
+    passport.authenticate("local", { 
+        failureRedirect: "/login", 
+        failureFlash: true 
+    }), 
+    async (req, res) => {
+        req.flash("success","Welcome back to StaySphere!");
+        res.redirect("/listings");
+});
+
+module.exports = router;
