e7418672e734491e618155dce9602f2e15523389
Asneh Raj <rajasneh91@gmail.com>
2024-12-25 20:56:51 +0530

    Fix: Add try-catch to isOwner and isReviewAuthor middleware, ensure next() is called

--- middleware.js
+++ middleware.js
@@ -21,13 +21,25 @@ module.exports.saveRedirectUrl = (req, res, next) => {
 };
 
 module.exports.isOwner = async (req, res, next) => {
-    let { id } = req.params;
-    let listing = await Listing.findById(id);
-    if (!listing.owner._id.equals(res.locals.currUser._id)) {
-        req.flash("error", "You are not the owner of the listing");
-        return res.redirect(`/listings/${id}`);
+    try {
+        const { id } = req.params;
+        const listing = await Listing.findById(id);
+        if (!listing) {
+            req.flash("error", "Listing not found!");
+            return res.redirect("/listings");
+        }
+        if (!listing.owner._id.equals(res.locals.currUser._id)) {
+            req.flash("error", "You are not the owner of the listing");
+            return res.redirect(`/listings/${id}`);
+        }
+        next(); // Call next() to proceed
+    } catch (err) {
+        console.error("Error in isOwner middleware:", err);
+        req.flash("error", "Something went wrong!");
+        return res.redirect("/listings");
     }
-}
+};
+
 
 module.exports.validateListing = (req, res, next) => {
     let { error } = listingSchema.validate(req.body);
@@ -51,10 +63,22 @@ module.exports.validateReview = (req, res, next) => {
 };
 
 module.exports.isReviewAuthor = async (req, res, next) => {
-    let { id,reviewId } = req.params;
-    let review = await Review.findById(reviewId);
-    if (!review.author.equals(res.locals.currUser._id)) {
-        req.flash("error", "You are not the author of this review");
+    try {
+        const { id, reviewId } = req.params;
+        const review = await Review.findById(reviewId);
+        if (!review) {
+            req.flash("error", "Review not found!");
+            return res.redirect(`/listings/${id}`);
+        }
+        if (!review.author.equals(res.locals.currUser._id)) {
+            req.flash("error", "You are not the author of this review");
+            return res.redirect(`/listings/${id}`);
+        }
+        next(); // Call next() to proceed
+    } catch (err) {
+        console.error("Error in isReviewAuthor middleware:", err);
+        req.flash("error", "Something went wrong!");
         return res.redirect(`/listings/${id}`);
     }
 };
+
