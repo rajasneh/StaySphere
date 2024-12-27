cc7de9977c8d15aff54b10140e0254fbcf9b8705
Asneh Raj <rajasneh91@gmail.com>
2024-12-27 20:29:59 +0530

    cloud setup and manipulating form

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV!="production"){
+    require(`dotenv`).config();
+}
+
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
@@ -10,6 +14,8 @@ const flash=require("connect-flash");
 const passport=require("passport");
 const LocalStrategy=require("passport-local");
 const User=require("./models/user.js");
+
+
 
 const listingRouter=require("./routes/listing.js");
 const reviewsRouter=require("./routes/review.js");
--- routes/listing.js
+++ routes/listing.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-
-
 const listingController = require("../controllers/listings.js");
+const multer=require("multer");
+const upload=multer({dest:`uploads/`})
 
 
 router
@@ -13,11 +13,14 @@ router
     //index route
     .get(wrapAsync(listingController.index))
     //create route
-    .post(
-        isLoggedIn,
-        validateListing,
-        wrapAsync(listingController.createListing)
-    );
+    // .post(
+    //     isLoggedIn,
+    //     validateListing,
+    //     wrapAsync(listingController.createListing)
+    // );
+    .post(upload.single(`listing[image]`),(req,res)=>{
+        res.send(req.file);
+    });
 
 //New route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
