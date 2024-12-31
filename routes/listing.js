f25699289afb2648f317168663468636eb067e32
Asneh Raj <rajasneh91@gmail.com>
2024-12-31 13:10:12 +0530

    create listing error solved

--- routes/listing.js
+++ routes/listing.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
-const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
 const multer=require("multer");
 const {storage}=require("../cloudConfig.js");
-const upload=multer({storage})
+const upload=multer({storage});
 
 
 router
@@ -16,8 +15,8 @@ router
     // create route
     .post(
         isLoggedIn,
-        validateListing,
         upload.single(`listing[image]`),
+        validateListing, 
         wrapAsync(listingController.createListing)
     );
 
