8311515d3124e254d8d19654a774bdef69b3f770
Asneh Raj <rajasneh91@gmail.com>
2025-01-01 12:53:43 +0530

    Map marker

--- public/js/map.js
+++ public/js/map.js
@@ -1,17 +1,16 @@
 let mapKey =mapKeyEjs; 
-
-// Default coordinates (example: Jamshedpur)
-const coordinates = { lat: 22.805618, lng: 86.203110 };
-
+Coordinates=[listingsCoordinates[1],listingsCoordinates[0]];
 // Initialize the map and set its view dynamically
-const map = L.map('map').setView([coordinates.lat, coordinates.lng], 9);
+const map = L.map('map').setView(Coordinates, 9);
 
 // Add MapTiler tiles
 L.tileLayer(`https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=${mapKey}`, {
     attribution: '&copy; <a href="https://www.maptiler.com/copyright/">MapTiler</a> contributors'
 }).addTo(map);
 
-// Add a marker at the dynamic center coordinates
-L.marker([coordinates.lat, coordinates.lng]).addTo(map)
+console.log(Coordinates);
+
+// Add a marker at the dynamic center coordinates with a custom icon
+L.marker(Coordinates).addTo(map)
     .bindPopup('Hello from Jamshedpur')
     .openPopup();
