b9b4d80585e4769dd65954775011cf9a82f5980d
Asneh Raj <rajasneh91@gmail.com>
2025-01-01 11:43:07 +0530

    Geocoding,Storing coordinates

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,5 +1,8 @@
 const Listing = require("../models/listing");
+const mapKey=process.env.MAP_KEY;
 
+const maptilerClient = require("@maptiler/client");
+maptilerClient.config.apiKey = mapKey;
 
 
 module.exports.index = async (req, res) => {
@@ -29,16 +32,23 @@ module.exports.showListing = async (req, res) => {
 }
 
 module.exports.createListing = async (req, res, next) => {
+    const result = await maptilerClient.geocoding.forward(req.body.listing.location);
+
+
     let url = req.file.path;
     let filename = req.file.filename;
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
     newListing.image = { url, filename };
-    await newListing.save();
-    req.flash("success", "New Listing Created!");
-    res.redirect("/listings")
-}
 
+    newListing.geometry=result.features[0].geometry;
+
+    let savedListing=await newListing.save();
+    console.log(savedListing,"----->LISTING SAVED")
+
+    req.flash("success", "New Listing Created!");
+    res.redirect("/listings");
+}
 module.exports.renderEditForm = async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
